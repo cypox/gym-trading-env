54973d95b158c452252c9b9a206fd9515faea20c
Cypox <am_neggaz@esi.dz>
2021-11-20 19:12:24 +0100

    use old data from oldest slice

--- main.py
+++ main.py
@@ -20,12 +20,10 @@ use_trained = True
 
 
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=NIO&interval=1min&slice=year2month12&apikey=E1HH8I7PMXVTAGS9'
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=NIO&interval=1min&slice=year1month1&apikey=E1HH8I7PMXVTAGS9'
+#url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=NIO&interval=1min&slice=year1month1&apikey=E1HH8I7PMXVTAGS9'
 df = pd.read_csv(url)
 
-#today = datetime.today().strftime('%Y-%m-%d')
-#df = web.DataReader('NIO', data_source='av-intraday', start='2021-10-01', end=today, api_key='E1HH8I7PMXVTAGS9')
-df.reset_index(inplace=True) # for some stupid reason we need this index column to be a number and not Date
+df.reset_index(inplace=True)
 df.sort_values('index', inplace=True)
 
 # The algorithms require a vectorized environment to run
@@ -33,7 +31,7 @@ env = DummyVecEnv([lambda: StockTradingEnv(df)])
 
 model = PPO('MlpPolicy', env, verbose=1)
 if use_trained:
-  model.load("nio-intraday-trained")
+  model.load("nio-intraday-trained", env=env)
 else:
   model.learn(total_timesteps=20000)
   model.save("nio-intraday-trained")
@@ -43,5 +41,5 @@ for i in range(2000):
   action, _states = model.predict(obs)
   #action = [env.action_space.sample()]
   obs, rewards, done, info = env.step(action)
-  print(rewards)
+  #print(rewards)
   env.render()
