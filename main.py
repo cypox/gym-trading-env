073644e174c2ba4a68c6cb9826b500bc9ec8878b
Cypox <am_neggaz@esi.dz>
2021-11-21 15:07:25 +0100

    added save, ts parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stable_baselines3 import A2C
+from stable_baselines3.common import evaluation
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
@@ -11,6 +12,9 @@ from data.importer import *
 
 use_trained = False
 update_data = False
+save_model = False
+evaluate_model = False
+training_timestamps = 10000
 
 if update_data:
   download_data()
@@ -23,10 +27,12 @@ model = A2C('MlpPolicy', env, verbose=1, tensorboard_log="./nio_trading_tensorbo
 if use_trained:
   model.load("nio-intraday-trained", env=env)
 else:
-  model.learn(total_timesteps=1000000)
-  model.save("nio-intraday-trained")
+  model.learn(total_timesteps=training_timestamps)
+  if save_model:
+    model.save("nio-intraday-trained")
 
-#mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+if evaluate_model:
+  mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 
 def dummy_strategy(obs):
   obs = obs[0]
